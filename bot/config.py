5b8f11084704569c9b6850c24ac4855500eda1ae
Watashi-TOBi <rasmisdead@gmail.com>
2026-06-08 19:07:58 +0530

    Add files via upload

---
+++ bot/config.py
@@ -0,0 +1,11 @@
+import os
+
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN") or os.environ.get("BOT_TOKEN")
+GROQ_API_KEY   = os.environ.get("GROQ_API_KEY")
+
+if not TELEGRAM_TOKEN:
+    raise RuntimeError("TELEGRAM_TOKEN environment variable is not set")
+
+SUPPORT_URL = os.environ.get("SUPPORT_URL", "https://t.me/bored_hub")
+OWNER_URL   = os.environ.get("OWNER_URL",   "https://t.me/Yours_Rasm")
+SOURCE_URL  = os.environ.get("SOURCE_URL",  "https://github.com/")
